e984955938f613e00bde49109c020ac61ea570aa
Graham Jerome <graham.jerome@gmail.com>
2019-09-04 21:12:31 -0400

    Create react app, and setup unsplash fetch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    images: []
+  }
+
+  componentDidMount() {
+    fetch(`https://api.unsplash.com/search/photos?client_id=15bf48e3b6c618e86ba481c00d7ee8d1a2d69e9ab9402ed84299ea765a8d0d14&page=1&query=coffee`)
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ images: data })
+      console.log(data)
+    })
+    .catch(console.log)
+  }
+
+  render () {
+    return (
+      <h2>Insert a Dog</h2>
+    );
+  }
 }
 
 export default App;
