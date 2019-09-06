6e3034c77dff24cdbaa225ec93dc8505ef7407a7
Graham Jerome <graham.jerome@gmail.com>
2019-09-06 14:45:43 -0400

    Added .env for API keys, and adjusted logic for query.

--- src/App.js
+++ src/App.js
@@ -4,26 +4,39 @@ import './App.css';
 
 class App extends Component {
   state = {
-    images: []
+    imageCategory: 'family',
+    imageSearchValue: null
   }
-
   componentDidMount() {
-    UnsplashAPI.getRandom()
-      .then( data => {
-        console.log(data)
-        let imageUrl = data.urls.small
-        this.updateBgImage(imageUrl)
-      })
+    this.updateBgImage()
   }
 
-  updateBgImage = (imageUrl) => {
-    document.body.style.backgroundImage = `url(${imageUrl})`
+  updateBgImage = (category = this.state.imageCategory) => {
+    UnsplashAPI.getRandom(category)
+      .then(imageUrl => {
+        document.body.style.backgroundImage = `url(${imageUrl})`
+      })
+      .catch(err => console.log(err))
+  }
+
+  handleSearchSubmit = (event) => {
+    event.preventDefault();
+    this.updateBgImage(this.state.imageSearchValue)
+  }
+
+  handleChange = (event) => {
+    this.setState({imageSearchValue: event.target.value})
   }
 
   render () {
     return (
-      <div>
-
+      <div className="container-fluid">
+        <form onSubmit={this.handleSearchSubmit}>
+          <div className="row p-3">
+            <input type="text" className="form-control form-control-lg col-4" onChange={this.handleChange} name="image-category" placeholder="Enter a category ..." />
+            <input type="submit" className="btn btn-primary ml-3" value="Bomb!" />
+          </div>
+        </form>
         <div className='cody-bomb'></div>
       </div>
     );
