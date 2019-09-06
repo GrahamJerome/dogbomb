0825aa2848a5850df57c0d4fce11ccf7f909e1df
Graham Jerome <graham.jerome@gmail.com>
2019-09-06 15:31:44 -0400

    tweaks to layout

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import * as UnsplashAPI from './UnsplashAPI'
 import './App.css';
 
 class App extends Component {
-  // Sets a default value on imageQuery for first load.
+  // Sets a default values on first load.
   state = {
+    dogImageUrl: 'https://i.imgur.com/8d7y9fp.png',
     imageQuery: 'family'
   }
 
   // Fire up the first random image on component mount.
   componentDidMount() {
+    document.body.querySelector('.dog-bomb').style.backgroundImage = `url(${this.state.dogImageUrl})`
     this.updateBgImage()
   }
 
@@ -23,29 +25,47 @@ class App extends Component {
       .catch(err => console.log(err))
   }
 
-  // When a search is triggered, it triggers an update of the background image.
+  updateDogImage = (url = this.state.dogImageUrl) => {
+    document.body.querySelector('.dog-bomb').style.backgroundImage = `url(${this.state.dogImageUrl})`
+  }
+
+  // When a search is triggered, it triggers an update of the background and dog image.
   // I have allowed the same query to be searched multiple times as it
   // will probably produce different results each query.
   handleSearchSubmit = (event) => {
     event.preventDefault();
-    this.updateBgImage(this.state.imageQuery)
+
+    let imageQuery = document.body.querySelector("input[name='image-query']").value
+    this.setState({imageQuery: imageQuery})
+    this.updateBgImage()
+
+    let dogImageUrl = document.body.querySelector("input[name='dog-image-url']").value
+    if (dogImageUrl !== this.state.dogImageUrl) {
+      this.setState({dogImageUrl: dogImageUrl})
+      this.updateDogImage()
+    }
   }
 
   // Update the imageQuery state value as the user types.
-  handleChange = (event) => {
+  handleQueryChange = (event) => {
     this.setState({imageSearchValue: event.target.value})
   }
 
   render () {
     return (
-      <div className="container-fluid">
-        <form onSubmit={this.handleSearchSubmit}>
-          <div className="row p-3">
-            <input type="text" className="form-control form-control-lg col-4" onChange={this.handleChange} name="image-query" placeholder="Enter a category ..." />
-            <input type="submit" className="btn btn-primary ml-3" value="Bomb!" />
+      <div>
+        <form className="dog-bomb-search form-inline p-3 bg-white" onSubmit={this.handleSearchSubmit}>
+          <div className="form-group mb-2 mb-md-0">
+            <label for="dog-image-url">Dog Image Url</label>
+            <input type="url" className="form-control mx-3" name="dog-image-url" value={this.state.dogImageUrl} placeholder="Dog image url" />
           </div>
+          <div className="form-group mb-2 mb-md-0">
+            <label for="image-query">Category</label>
+            <input type="text" className="form-control mx-3" name="image-query" value={this.state.imageQuery} placeholder="Image category" />
+          </div>
+          <button type="submit" className="btn btn-primary mb-2">Bombs away!</button>
         </form>
-        <div className='cody-bomb'></div>
+        <div className='dog-bomb'></div>
       </div>
     );
   }
