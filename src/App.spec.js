378c857ae732d696e25ff6f914dae96a0576bc43
Graham Jerome <graham.jerome@gmail.com>
2019-09-09 11:00:32 -0400

    final...2

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,6 +1,5 @@
 import React from "react";
 import App from './App'
-// import { create } from "react-test-renderer";
 import { mount, configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
